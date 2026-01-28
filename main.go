d9eeac13dde413a7af8535f474cced4d74f9765f
rafly <raflyade9@gmail.com>
2026-01-28 22:33:24 +0700

    soal 2

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	sortCharacters()
+	// sortCharacters()
+	psbb()
 }
 
 func sortCharacters() {
@@ -27,7 +30,7 @@ func sortCharacters() {
 	//sanitize input
 	inputTrim := strings.Join(strings.Fields(input), "")
 	inputLower := strings.ToLower(inputTrim)
-	
+
 	//loop the input and search if its in the vowel
 	for i := 0; i < len(inputLower); i++ {
 		char := string(inputLower[i])
@@ -42,4 +45,46 @@ func sortCharacters() {
 
 	fmt.Println("\nVowel Characters:", resultVowels)
 	fmt.Println("Constant Characters:", resultConstant)
+}
+
+func psbb() {
+	reader := bufio.NewReader(os.Stdin)
+	//variable to store input 
+	var n int
+	fmt.Print("Input the number of families: ")
+	fmt.Scan(&n)
+
+	fmt.Print("Input the number of members in the family (separated by a space): ")
+	//read
+	line, _ := reader.ReadString('\n')
+	//convert it to array string
+	part := strings.Fields(line)
+	//validation 
+	if len(part) != n {
+		fmt.Println("Input must be equal with count of family")
+		return
+	}
+	
+	families := make([]int, n)
+	//scan through families 
+	for i := 0; i < n; i ++ {
+		families[i], _ = strconv.Atoi(part[i])
+	}
+
+
+	//sort the families array from low to high
+	sort.Ints(families)
+
+	left, right := 0, n-1
+	buses := 0
+
+	for left <= right {
+		if families[left]+families[right] <= 4 {
+			left++
+		}
+		right--
+		buses++
+	}
+
+	fmt.Printf("Minimum bus required is: %d\n", buses)
 }
