fc330b65fec5d568e53b2a6866141903ab9397cb
rafly <raflyade9@gmail.com>
2026-01-28 21:59:33 +0700

    feat: soal1

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	log.Printf("abc")
+	sortCharacters()
+}
+
+func sortCharacters() {
+	//variable to store word
+	var resultConstant string
+	var resultVowels string
+
+	//variable to store vowel
+	vowel := "aiueo"
+
+	fmt.Print("Input one line of words (S): ")
+	//scan input
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	//sanitize input
+	inputTrim := strings.Join(strings.Fields(input), "")
+	inputLower := strings.ToLower(inputTrim)
+	
+	//loop the input and search if its in the vowel
+	for i := 0; i < len(inputLower); i++ {
+		char := string(inputLower[i])
+
+		if strings.Contains(vowel, char) {
+			resultVowels += char
+		} else {
+			resultConstant += char
+		}
+		
+	}
+
+	fmt.Println("\nVowel Characters:", resultVowels)
+	fmt.Println("Constant Characters:", resultConstant)
 }
